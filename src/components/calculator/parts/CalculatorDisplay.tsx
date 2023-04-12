6b49b6ee53398be49c81b2ca0641fec1c0efdb3a
rifqimfahmi <rifqi416@gmail.com>
2023-04-12 22:40:52 +0800

    move file

--- src/components/calculator/parts/CalculatorDisplay.tsx
+++ src/components/calculator/parts/CalculatorDisplay.tsx
@@ -1,7 +1,7 @@
 const CalculatorDisplay = () => {
   return (
-    <div className="flex">
-      <input className="h-64 flex-grow" value={0} disabled/>
+    <div>
+      <input className="w-full" value={0} disabled/>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Header from "../../components/Header";
-import Calculator from "../../components/calculator/Calculator";
-import Footer from "../../components/Footer";
+import Header from "@/components/Header";
+import Calculator from "@/components/calculator/Calculator";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
